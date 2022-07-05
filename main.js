d35a4b3f095fc1a9752454d7e59a65559a7d7a78
song1227jg <song1227jg@hectonproject.com>
2022-07-05 15:21:45 +0900

    JS-Transparent navbar

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// navbar 메뉴 스크롤에 따라 투명도 적용
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar__dark");
+  } else {
+    navbar.classList.remove("navbar__dark");
+  }
+});
