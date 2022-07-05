b17c23158bafa385f60eaa400db39046eff4e02d
song1227jg <song1227jg@hectonproject.com>
2022-07-05 16:19:04 +0900

    JS-Scroll to section

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar__dark");
   }
 });
+
+// 버튼 클릭 시 스크롤 위치 변경
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link == null) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth", block: "center" });
+});
